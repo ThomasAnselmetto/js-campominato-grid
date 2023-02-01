82e2d475cb9c0561e90750bf56a4bcc21fbc2381
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-02-01 15:37:11 +0100

    2nd commit

---
+++ JS/main.js
@@ -0,0 +1,55 @@
+//  L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+
+/****************************************************
+*                                                   *
+*                       CODE ON LOAD                *
+*                                                   *    
+* ***************************************************/
+const buttonStart = document.getElementById("start-game");
+
+
+
+
+
+
+
+
+
+/****************************************************
+*                                                   *
+*                       FUNCTION                    *
+*                                                   *    
+* ***************************************************/
+// creare una funzione che costruisca la griglia creando degli elementi nel HTML
+const gridEl = document.getElementById("grid");
+createGrid(gridEl);
+
+function createGrid(griglia) {
+    // genero un elemento presumibilmente un div
+
+    // gli aggiungo la classe per poterlo vedere a schermo
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
